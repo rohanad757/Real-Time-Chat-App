58bf998a3053e6192eb237426f665d187e7cee58
Rohan <105427470+rohanad757@users.noreply.github.com>
2025-04-05 06:25:43 +0530

    Update AuthController.js

--- Server/controllers/AuthController.js
+++ Server/controllers/AuthController.js
@@ -1,6 +1,6 @@
 import User from "../models/userModel.js";
 import jwt from 'jsonwebtoken';
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 
 const maxAge = 3 * 24 * 60 * 60;
 const createToken = ( id ) => {
