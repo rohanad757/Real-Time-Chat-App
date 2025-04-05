84557811bdfa5f006fdb97c5797ee4c124605428
Rohan <105427470+rohanad757@users.noreply.github.com>
2025-04-05 06:59:16 +0530

    Update socket.js

--- Server/socket.js
+++ Server/socket.js
@@ -7,7 +7,7 @@ const httpServer = createServer(app);
 
 const io = new Server(httpServer, {
     cors: {
-        origin: 'http://localhost:5173', // Adjust this based on your frontend URL
+        origin: process.env.ORIGIN || 'http://localhost:5173', // Adjust this based on your frontend URL
         methods: ['GET', 'POST'],
         credentials: true,
     },
